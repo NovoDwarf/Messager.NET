33c1e6092ffd7431af6f9f85a622cfe8e2e6be5d
d1rknwh1te3 <kirillstarodub310@gmail.com>
2025-11-16 01:08:54 +0300

    Add not implemented Microsoft DI extension

--- Sources/Messager.NET/Extensions/ContainerBuilderExtensions.cs
+++ Sources/Messager.NET/Extensions/ContainerBuilderExtensions.cs
@@ -1,10 +1,21 @@
 ﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Messager.NET.Extensions;
 
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddMessager(this IServiceCollection services, Action<MessagerOptions>? configureOptions = null)
+	{
+		// TODO: add injection method for Microsoft.Extensions.DependencyInjection;
+		
+		return services;
+	}
+}
+
 public static class ContainerBuilderExtensions
 {
-	public static ContainerBuilder AddEventSystem(this ContainerBuilder builder, Action<MessagerOptions>? configureOptions = null)
+	public static ContainerBuilder AddMessager(this ContainerBuilder builder, Action<MessagerOptions>? configureOptions = null)
 	{
 		var options = new MessagerOptions();
 		
