84f84cfc886a01cdf05f9ec6f36319f1b23094bc
d1rknwh1te3 <kirillstarodub310@gmail.com>
2025-11-16 01:09:39 +0300

    Move ServiceCollection extensions to own class file

--- Sources/Messager.NET/Extensions/ContainerBuilderExtensions.cs
+++ Sources/Messager.NET/Extensions/ContainerBuilderExtensions.cs
@@ -1,17 +1,6 @@
 ﻿using Autofac;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Messager.NET.Extensions;
-
-public static class ServiceCollectionExtensions
-{
-	public static IServiceCollection AddMessager(this IServiceCollection services, Action<MessagerOptions>? configureOptions = null)
-	{
-		// TODO: add injection method for Microsoft.Extensions.DependencyInjection;
-		
-		return services;
-	}
-}
 
 public static class ContainerBuilderExtensions
 {
---
+++ Sources/Messager.NET/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Messager.NET.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddMessager(this IServiceCollection services, Action<MessagerOptions>? configureOptions = null)
+	{
+		// TODO: add injection method for Microsoft.Extensions.DependencyInjection;
+		
+		return services;
+	}
+}
